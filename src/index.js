17c7e823dc760383e5d41709ff3a331fef871c81
Johnny Wright <johnnywrightiv@gmail.com>
2023-01-02 14:59:29 -0600

    Create videoSearch func to store search term and pass to API request (note: runs with axios.catch(error)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import SearchBar from './components/search_bar';
 import reportWebVitals from './reportWebVitals';
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+const axios = require('axios');
 
 const App = () => {
-  return <h1>Heyyo world!</h1>
+  // Passing (search) term to axios API request
+  const videoSearch = term => {
+    // define base url
+    const url = 'https://www.googleapis.com/youtube/v3/search';
+
+    // define params object of url variables for API request
+    const params = {
+      part: 'snippet',
+      key: API_KEY,
+      q: term,
+      type: 'video',
+    };
+
+    // use axios to RETURN A PROMISE based on API response
+    axios.get(url, { params: params })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <SearchBar onSearchTermChange={videoSearch} />
+    </div>
+  )
+
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
